8505a6d10a680717366c66149bc34a33d1e8907d
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-02-11 22:07:34 +0000

    Done first returns and parameters task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,27 +1,38 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func printRoomInro() {
-    print("This program calculates room area and volume.")
-    print("It also finds the usable space after furniture.")
+// Defines a function to calcute the area of a room when given its length and width
+func roomArea(length: Double, width: Double) -> String {
+    // Calculates the area of the room with the given values
+    let area = length * width
+    // Returns a message telling the user what the area of the room is
+    return "The area of the room is \(area)m²."
 }
 
-func printDivider() {
-    print("--------")
+// Defines a function to calcute the volume of a room when given its length and width
+func roomVolume(length: Double, width: Double, height: Double) -> String {
+    // Calculates the volume of the room with the given values
+    let volume = length * width * height
+    // Returns a message telling the user what the area of the room i
+    return "The volume of the room is \(volume)m³."
 }
 
-func printUnitsNote() {
-    print("All measurements are in metres.")
-}
-
-func printGoodbye() {
-    print("Done. Thanks for using the calculator")
-}
 
 @main
 struct SwiftPlayground {
     static func main() {
-            printGoodbye()
-        }
+        
+        // Declares the length and width of a small room and uses the function to calculate its area
+        let smallRoomArea = roomArea(length: 5, width: 10)
+
+        // Declares the length and width of a small room and uses the function to calculate its volume
+        let largeRoomVolume = roomVolume(length: 20, width: 40, height: 5)
+
+        // Displays the message given by the function
+        print(smallRoomArea)
+
+        // Displays the message given by the function
+        print(largeRoomVolume)
     }
+}
 
