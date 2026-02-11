68e5c58f0b42b431e44f0fa517ce5a9b96e400d2
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-02-11 21:31:53 +0000

    Done function task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,49 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func printRoomInro() {
+    print("This program calculates room area and volume.")
+    print("It also finds the usable space after furniture.")
+}
+
+func printDivider() {
+    print("--------")
+}
+
+func printUnitsNote() {
+    print("All measurements are in metres.")
+}
+
+func printGoodbye() {
+    print("Done. Thanks for using the calculator")
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let dimensions: [String] = ["length", "width", "Height"]
-        var dimensionValues: [Double] = []
-        var currentDimetion = 0
-        var addingFurniture = true
-        while currentDimetion < 3 {
-            print("Enter room \(dimensions[currentDimetion]) in metres.")
-            if let input = readLine(), let dimension = Double(input) {
-                if dimension >= 1{
-                    dimensionValues.append(dimension)
-                currentDimetion = currentDimetion + 1
-                } else {
-                    print("Invaid response, please enter a positive number")
-                }
-
-            } else {
-            print("Invaid response, please enter a positive number")
-
-            }
-        }
-
-        let roomArea = dimensionValues[0] * dimensionValues[1]
-
-        let roomVolume = roomArea * dimensionValues[2] 
-
-        print("The area of your room is \(roomArea)m².")
-
-        print("The volume of your room is \(roomVolume)m³.")
-
-        while addingFurniture == true {
-            print("Would you like to add a piece of furniture to your room? (y/n)")
-            if let imput = readLine(), let response = String(imput) {
-                if response == "y" {
-                    print
-                } else if response == "n" {
-                    addingFurniture = false
-                }
-            } else {
-                print("Invalid imput. If you want to add a piece of furniture type y, if not type n")
-            }
+            printGoodbye()
         }
     }
-}
+
