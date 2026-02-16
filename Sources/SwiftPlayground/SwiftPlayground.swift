f7611e27817c2ef934ed8ce63c99d2adfd66a346
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-02-16 23:51:15 +0000

    Done scope task but for real this time

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,19 +2,25 @@
 // https://docs.swift.org/swift-book
 
 
+
 @main
 struct SwiftPlayground {
     static func main() {
+
+        // This bool is to decide the outcome of the next if statement 
         let isLarge = true
 
+        
         if isLarge == true {
             let volume = 100
             print(volume)
         } else {
+            // Since the bool is true this code will not run
             let volume = 20
             print(volume)
         }
 
+        // Since the variable was declared in a if statement the program is unable to find the costant
         print(volume)
     }
 }
