a1ad590c6b7daf56b594abbf8875ccb1ec197f5c
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-02-18 22:03:01 +0000

    Help

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,48 +2,42 @@
 // https://docs.swift.org/swift-book
 
 
+func menuChoice() -> Int {
+    var displayedMenu = 0
+    var menuChoiceRunning = true
+    while menuChoiceRunning {
+        print("==== Egg Shop ====")
+        print("    1. Add eggs")
+        print("    2. Sell eggs")
+        print("    3. Show current stock")
+        print("    4. Show total eggs sold")
+        print("    5. Exit")
+        print("    Choose an option:")
+        if let response = readLine(),let currentMenu = Int(response) {
+            if currentMenu <= 5 && currentMenu >= 1 {
+                displayedMenu = currentMenu
+                menuChoiceRunning = false
+            } else {
+                print("Please read the above text and enter a valid number to prosses your request")
+            }
+        } else {
+        print("Please read the above text and enter a valid number to prosses your request")
+        }
+    }
+    
+return displayedMenu
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        let dimensions: [String] = ["length", "width", "Height"]
-        var dimensionValues: [Double] = []
-        var currentDimetion = 0
-        var addingFurniture = true
-        while currentDimetion < 3 {
-            print("Enter room \(dimensions[currentDimetion]) in metres.")
-            if let input = readLine(), let dimension = Double(input) {
-                if dimension >= 1{
-                    dimensionValues.append(dimension)
-                currentDimetion = currentDimetion + 1
-                } else {
-                    print("Invaid response, please enter a positive number")
-                }
-
-            } else {
-            print("Invaid response, please enter a positive number")
-
-            }
-        }
-
-        let roomArea = dimensionValues[0] * dimensionValues[1]
-
-        let roomVolume = roomArea * dimensionValues[2] 
-
-        print("The area of your room is \(roomArea)m².")
-
-        print("The volume of your room is \(roomVolume)m³.")
-
-        while addingFurniture == true {
-            print("Would you like to add a piece of furniture to your room? (y/n)")
-            if let imput = readLine(), let response = String(imput) {
-                if response == "y" {
-                    print
-                } else if response == "n" {
-                    addingFurniture = false
-                }
-            } else {
-                print("Invalid imput. If you want to add a piece of furniture type y, if not type n")
-            }
-        }
+        print(menuChoice())
+        // var runningEggShop = true
+        // while runningEggShop == true {
+        //     let displayedMenu = menuChoice()
+        //     if displayedMenu == 1 {
+                
+        //     }
+        // }
     }
 }
