e249be8484cdc6e10352ea43ba70520ab40cdbdf
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-03-11 22:13:31 +0000

    Updated

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,40 +1,74 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// // The Swift Programming Language
+// // https://docs.swift.org/swift-book
 
-func  maxValue(in table: [[Double]]) -> Double! {
-    var largestNumber = 0.0
-    var arrayEmpty = true
-    for row in table {
-        for value in row {
-            if largestNumber < value {
-                largestNumber = value
-                arrayEmpty = false
-            }
+
+// Task d
+// func  maxValue(in table: [[Double]]) -> Double! {
+//     var largestNumber = 0.0
+//     var arrayEmpty = true
+//     for row in table {
+//         for value in row {
+//             if largestNumber < value {
+//                 largestNumber = value
+//                 arrayEmpty = false
+//             }
+//         }
+//     }
+//     if arrayEmpty == false {
+//         return largestNumber
+//     } else {
+//         return nil
+//     }
+// }
+
+// @main
+// struct SwiftPlayground {
+//     static func main() {
+//         let readings = [
+//             [1.5, 3.2, 2.8],
+//             [7.1],
+//             [4.4, 6.0],
+//             [5.9, 8.3, 0.7, 2.2]
+//         ]
+
+//         let largestValue = maxValue(in: readings)
+        
+//         if largestValue == nil {
+//             print("This 2D array is empty")
+//         } else {
+//             print("The largest number in this 2D array is \(largestValue)")
+//         }
+//     }
+// }
+
+func columnTotal(in table: [[Int]], column: Int) -> Int {
+    var average = 0
+    var numberAmounts = 0
+    (0...3).forEach { row in
+        if table.indices.contains(row) && table[row].indices.contains(column) {
+            average = average + table[row][column]
+            numberAmounts = numberAmounts + 1
         }
+    
     }
-    if arrayEmpty == false {
-        return largestNumber
-    } else {
-        return nil
-    }
+    return average/numberAmounts
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let readings = [
-            [1.5, 3.2, 2.8],
-            [7.1],
-            [4.4, 6.0],
-            [5.9, 8.3, 0.7, 2.2]
+        let table = [
+        [3, 5, 7, 9],
+        [2, 4],
+        [8, 6, 1],
+        [10]
         ]
 
-        let largestValue = maxValue(in: readings)
-        
-        if largestValue == nil {
-            print("This 2D array is empty")
-        } else {
-            print("The largest number in this 2D array is \(largestValue)")
+        var column = 0
+
+        while column <= 3 {
+            print(columnTotal(in: table, column: column))
+            column = column + 1
         }
     }
 }
