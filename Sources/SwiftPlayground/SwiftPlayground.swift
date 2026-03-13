58c91bdcd7d9bf760df65c5cd6957ecb754b06e6
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-03-13 02:01:46 +0000

    I don't undersand anything

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,49 +1,50 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/// Parameter:
+/// - board: The 2D grid to display.
+func printBoard(_ board: [[String]]) {
+    for row in board {
+        print(row)
+    }
+}
+
+// Parameters:
+/// - row: The row index for the guess.
+/// - col: The column index for the guess.
+/// - ocean: The hidden ships grid.
+/// - guesses: The player's current guesses grid.
+///
+/// Returns: The updated guesses grid after the guess is applied.
+func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
+    
+}
+
+func getTarget (guesses: [[String]],) -> (Int, Int){
+     
+    print("Where would you like to guess on the y axis?")
+    if let response = readLine(), let xGuess = Int(response) {
+    }
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let dimensions: [String] = ["length", "width", "Height"]
-        var dimensionValues: [Double] = []
-        var currentDimetion = 0
-        var addingFurniture = true
-        while currentDimetion < 3 {
-            print("Enter room \(dimensions[currentDimetion]) in metres.")
-            if let input = readLine(), let dimension = Double(input) {
-                if dimension >= 1{
-                    dimensionValues.append(dimension)
-                currentDimetion = currentDimetion + 1
-                } else {
-                    print("Invaid response, please enter a positive number")
-                }
+        let size = 6
+        var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
+        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+        var gameRunning = true
+        ocean[1][3] = "S"
+        ocean[2][3] = "S"
+        ocean[4][0] = "S"
+        ocean[5][4] = "S"
+        printBoard(guesses)
+        while gameRunning == true {
+            print("Where would you like to guess on the y axis?")
+            printBoard(guesses)
+            print("Where would you like to guess on the y axis?")
 
-            } else {
-            print("Invaid response, please enter a positive number")
-
-            }
-        }
-
-        let roomArea = dimensionValues[0] * dimensionValues[1]
-
-        let roomVolume = roomArea * dimensionValues[2] 
-
-        print("The area of your room is \(roomArea)m².")
-
-        print("The volume of your room is \(roomVolume)m³.")
-
-        while addingFurniture == true {
-            print("Would you like to add a piece of furniture to your room? (y/n)")
-            if let imput = readLine(), let response = String(imput) {
-                if response == "y" {
-                    print
-                } else if response == "n" {
-                    addingFurniture = false
-                }
-            } else {
-                print("Invalid imput. If you want to add a piece of furniture type y, if not type n")
+            
             }
         }
     }
-}
