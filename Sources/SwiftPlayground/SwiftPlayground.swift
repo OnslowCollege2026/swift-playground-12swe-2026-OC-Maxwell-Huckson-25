584bb752623d543d4fb6795153f12df873daf56f
OC-Maxwell-Huckson-25 <maxwell.huckson@student.onslow.school.nz>
2026-03-18 21:51:18 +0000

    trynig to debug

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,15 +1,71 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
+import Foundation
+@main
+struct SwiftPlayground {
+    static func main() throws {
+        // The maximum number of guesses the user can make.
+        let maximumGuesses = 5
+        
+        // The size of the board (width and height).
+        let size = 6
+        
+        // The board you are playing on.
+        var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
+        
+        // A record of the guesses that have been made.
+        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+        
+        // Manually place the ships (testing version).
+        ocean[1][3] = "S"
+        ocean[2][3] = "S"
+        ocean[4][0] = "S"
+        ocean[5][4] = "S"
+        
+        printBoard(guesses)
+        
+        // Allow the user to make a certain number of guesses.
+        var progress = 1
+        while progress <= maximumGuesses {
+            // Ask for the row and column number.
+            print("\(progress): Please enter a row number, press Enter, then a column number: ")
+            
+            // Get the user's input for both the row and column.
+            // If the input is not usable, tell them to try again.
+            guard let userInput = readLine(),
+                let row = Int(userInput),
+                let userInput2 = readLine(),
+                let col = Int(userInput2) else {
+                print("Your guess is invalid. Try again.")
+                continue
+            }
+            
+            // By this point in the code, the row and col must be valid integers.
+            // Use processGuess to check that they are also valid guesses.
+            guesses = processGuess(row: row, col: col, ocean: ocean, guesses: guesses)
+            
+            // Print the updated guesses board, then go to the next move.
+            printBoard(guesses)
+            progress = progress + 1
+        }
+    }
+}
 /// Parameter:
 /// - board: The 2D grid to display.
 func printBoard(_ board: [[String]]) {
-    for row in board {
-        print(row)
+    var columnLabels = "  "
+    for i in 1...board.count {
+        columnLabels = columnLabels + "\(i) "
+    }
+    print(columnLabels)
+    
+    for (index, row) in board.enumerated() {
+        var rowString = "\(index + 1) "
+        for cell in row {
+            rowString = rowString + cell + " "
+        }
+        print(rowString)
     }
 }
-
-// Parameters:
+/// Parameters:
 /// - row: The row index for the guess.
 /// - col: The column index for the guess.
 /// - ocean: The hidden ships grid.
@@ -17,34 +73,60 @@ func printBoard(_ board: [[String]]) {
 ///
 /// Returns: The updated guesses grid after the guess is applied.
 func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
-    
-}
-
-func getTarget (guesses: [[String]],) -> (Int, Int){
-     
-    print("Where would you like to guess on the y axis?")
-    if let response = readLine(), let xGuess = Int(response) {
+    // Make sure the row and column exist. If not, exit this function early.
+    guard row >= 1, row <= ocean.count, col >= 1, col <= ocean[0].count else {
+        print("Your guess is invalid. Try again.")
+        return guesses
     }
+    
+    // Make sure that the user hasn't already guessed the position.
+    // If not, exit this function early.
+    guard guesses[row - 1][col - 1] != "O" || guesses[row - 1][col - 1] != "X" else {
+        print("You have already guessed that position. Try again.")
+        return guesses
+    }
+    
+    // Make sure that the user hasn't missed the battleship.
+    // If not, return an updated guesses table including the miss.
+    guard guesses[row - 1][col - 1] == "~" else {
+        print("MISS!")
+        var newGuesses = guesses
+        newGuesses[row - 1][col - 1] = "O"
+        return newGuesses
+    }
+    
+    // If the code hasn't returned by now, the player must have hit a ship.
+    print("You've sunk my battleship!")
+    var newGuesses = guesses
+    newGuesses[row - 1][col - 1] = "X"
+    return newGuesses
 }
-
-@main
-struct SwiftPlayground {
-    static func main() {
-        let size = 6
-        var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
-        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
-        var gameRunning = true
-        ocean[1][3] = "S"
-        ocean[2][3] = "S"
-        ocean[4][0] = "S"
-        ocean[5][4] = "S"
-        printBoard(guesses)
-        while gameRunning == true {
-            print("Where would you like to guess on the y axis?")
-            printBoard(guesses)
-            print("Where would you like to guess on the y axis?")
-
-            
+/// Parameters:
+/// - ocean: The hidden ships grid.
+/// - guesses: The player's current guesses grid.
+///
+/// Returns: How many ships remain unhit.
+func remainingShips(in ocean: [[String]], guesses: [[String]]) -> Int {
+    var shipsCount = 0
+    for row in 0...ocean.count-1 {
+        for col in 0...row {
+            if ocean[row][col] == "S" {
+                shipsCount = shipsCount + 1
             }
         }
     }
+    
+    var hitCount = 0
+    for row in 0...guesses.count-1 {
+        for col in 0...row {
+            if guesses[row][col] == "X" {
+                hitCount = hitCount + 1
+            }
+        }
+    }
+    
+    return shipsCount - hitCount
+}
+
+
+
